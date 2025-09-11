3d1bf3d8839051164a1f9d7ee81a4505819ec274
Yeerapaneni Anurag <anuragchowdary2004@gmail.com>
2025-09-11 12:58:26 +0530

    6,7,8 problems done in single file

---
+++ Basic/6_7_8.py
@@ -0,0 +1,21 @@
+#unioun of 2 sets
+a = {3,2,4,5,6,7,8}
+b= {4,12,5,1,6,8}
+
+c = a.union(b)
+print(c)
+
+
+#intersection of 2 sets
+a = {3,2,4,5,6,7,8}
+b= {4,12,5,1,6,8}
+c = a.intersection(b)
+print(c)
+
+
+
+#difference of 2 sets
+a = {1,12,2,6,7,8}
+b= {15,0,1,3,6}
+c = a.difference(b)
+print(c)
