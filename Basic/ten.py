d84a26d35d02fb88c5dc2a1f2e31774521e500b6
Yeerapaneni Anurag <anuragchowdary2004@gmail.com>
2025-09-11 15:00:14 +0530

    10th problem done

---
+++ Basic/ten.py
@@ -0,0 +1,15 @@
+telugu = int(input("enter your marks telugu : "))
+english = int(input("enter your marks english: "))
+hindi = int(input("enter your marks hindi : "))
+maths = int(input("enter your marks maths : "))
+science = int(input("enter your marks science  : "))
+social = int(input("enter your marks  social : "))
+
+
+total_marks = telugu + english + hindi + maths + science + social
+print("your total marks are :",total_marks)
+average_marks = total_marks / 6 
+print("your avg marks are : " , average_marks)
+
+
+
