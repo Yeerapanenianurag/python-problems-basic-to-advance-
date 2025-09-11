e940e6ee1340f54cefde4c561e5c42998460df24
Yeerapaneni Anurag <anuragchowdary2004@gmail.com>
2025-09-11 12:51:17 +0530

    5th problem done

---
+++ Basic/five.py
@@ -0,0 +1,15 @@
+a = int(input("enter 1st number :"))
+b = int(input("enter 2nd number :"))
+
+print("sum of 2 numbers is : " + str(a+b))
+
+print("product  of 2 numbers is :"+ str(a*b))
+
+print("division of 2 numbers is :"+str(a/b))
+
+print("difference of 2 numbers is :"+str(a-b))
+
+# print("square of 2 numbers is :"+str (a**b))
+
+print("modulous of 2 numbers is :"+ str(a%b) )
+
